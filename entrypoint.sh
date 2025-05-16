e85f9362cab5a8d9913374c7a70a17ec7a48b018
Liam Marwood <32343222+Liammarwood@users.noreply.github.com>
2025-05-16 21:00:19 +1000

    Update entrypoint.sh
    Removed the pushing to GitHub so it allows more generic use

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,9 +18,3 @@
 java -jar /flow-diff.jar $1 $2 >> /github/workspace/diff.txt
 
 OUTPUT=$(cat /github/workspace/diff.txt | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed 's/\t/    /g' | sed ':a;N;$!ba;s/\n/\\n/g')
-
-curl -X POST \
-     -H "Authorization: Token $3" \
-     -H "Accept: application/vnd.github+json" \
-     https://api.github.com/repos/$4/issues/$5/comments \
-     -d "{\"body\":\"$OUTPUT\"}"
